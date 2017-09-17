641f08e3463c313112cb330d64542636cf1e3357
andrei donati <andrei donati>
2017-09-17 09:49:24 -0300

    modify a view

--- teste/views.py
+++ teste/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world! This is our first view.")
+    return HttpResponse("Hello world! This is our second view.")
 # Create your views here.
