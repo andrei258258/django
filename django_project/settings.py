cb55dbfef51332ad9e3130c32da6a57aee2b6ccb
andrei donati <andrei donati>
2017-09-22 23:30:22 -0300

    ajust post_list.html

--- django_project/settings.py
+++ django_project/settings.py
@@ -58,6 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(PACKAGE_ROOT, 'templates')],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +122,8 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
 ]
 
 
--- teste/views.py
+++ teste/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
+from .models import Post
+from django.utils import timezone
+
 
 def index(request):
     return HttpResponse("Hello world! This is our second view.")
 
 
 def post_list(request):
-	return render(request, 'teste/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'teste/post_list.html', {'posts': posts})
 
 
 def teste(request):
